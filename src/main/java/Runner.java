c899c5f741e8c6b73cc24346f88e809220062204
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-16 01:37:43 +0100

    5. Remove Tresco from the list by name

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -37,8 +37,10 @@ public class Runner {
         System.out.println(scottishIslands);
 
 //        4. Print out the index position of "Skye"
+        System.out.println("Index of Skye:"+scottishIslands.indexOf("Skye"));
 
 //        5. Remove "Tresco" from the list by name
+
 //        6. Remove "Arran" from the list by index
 //        7. Print the number of islands in your arraylist
 //        8. Sort the list alphabetically
